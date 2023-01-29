33977223a6f31f6fa990bff187cb2227b21ffae7
mhanuz <46758023+mhanuz@users.noreply.github.com>
2023-01-29 19:24:53 +0600

    Update server-element.component.ts

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit,Input,ViewEncapsulation } from '@angular/core';
-
+// ViewEncapsulation:  Don't affect other elements in the application
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.Emulated // shadow dom injection
+  encapsulation: ViewEncapsulation.Emulated // style main HTML of this component.
   // None: get rid from shadow dom
 })
 export class ServerElementComponent implements OnInit {
   // bydefault all properties are accessible component itself, not other components
-  // using decorators, it tells the component allow acces properties from Parent component
-  @Input('servElementToSE') element : { // servElement: property binding alias
+  // using input decorators, it tells the angular allow access properties from Parent component
+  @Input('servElementToSE') element : { // servElementToSE: property binding alias
     name: string;
     type: string;
     content: string;
